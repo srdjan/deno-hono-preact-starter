483fc6f9a968aa83539567e8e0d2961476a46cae
Srdjan <srdjan@timok.com>
2023-01-14 22:01:49 -0500

    cleanup

--- server.ts
+++ server.ts
@@ -6,7 +6,11 @@ import { api } from './src/api/api.ts'
 const app = new Hono()
 
 app.use('*', logger(), prettyJSON())
+app.all('/favicon.ico', serveStatic({ path: './public/favicon.ico' }))
 app.use('*', serveStatic({ root: './public' }))
 app.route('/api', api)
-
+app.onError((err, c) => {
+  console.error(`${err}`)
+  return c.text('Custom Error Message', 500)
+})
 serve(app.fetch)
