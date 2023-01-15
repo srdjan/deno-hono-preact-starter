9ce1c260deb68169e55129d3d2349dc07d79a5cd
Srdjan <srdjan@timok.com>
2023-01-15 17:24:08 -0500

    refactoring

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,8 @@
 import { render, h } from 'preact'
 import { Redirect, Switch, Route, Link, useRoute } from "wouter-preact";
-import Counter from './components/Counter.tsx'
-import About from './components/About.tsx'
-import Features from './components/Features.tsx'
-import NotFound from './components/NotFound.tsx'
+import Counter from '../components/Counter.tsx'
+import About from './About.tsx'
+import Features from './Features.tsx'
 
 const ActiveLink = (props) => {
   const [isActive] = useRoute(props.href);
@@ -29,6 +28,8 @@ function App() {
 
       <main>
         <Switch>
+          <Route path="~/" children={<Redirect to="/" />} />
+
           <Route path="/">
             <Counter></Counter>
           </Route>
@@ -37,9 +38,6 @@ function App() {
           </Route>
           <Route path="/about">
             <About></About>
-          </Route>
-          <Route path="/:anything*">
-            <NotFound></NotFound>
           </Route>
         </Switch>
       </main>
